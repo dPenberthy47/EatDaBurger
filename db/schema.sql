6cb4be98491de04636b2ad564910349da6488509
dPenberthy47 <dp47@tamu.edu>
2018-09-22 23:54:25 -0500

    fixed my SQL table

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE burgers_db;
 
-USE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT;
+    id int NOT NULL AUTO_INCREMENT,
     name VARCHAR(250) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured boolean not null default 0,
     PRIMARY KEY (id)
-)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO burgers VALUES("hamburger", "TRUE"),
-INSERT INTO burgers VALUES("cheeseburger", "TRUE"),
-INSERT INTO burgers VALUES("double cheeseburger", "FALSE"),
-INSERT INTO burgers VALUES("bacon cheeseburger", "TRUE"),
-INSERT INTO burgers VALUES("jalepino cheeseburger", "FALSE"),
-INSERT INTO burgers VALUES("patty melt", "FALSE"),
-INSERT INTO burgers VALUES("steak sandwhich", "FALSE"),
-INSERT INTO burgers VALUES("bbq burger", "FALSE"),
+INSERT INTO burgers (name, devoured) VALUES ("hamburger", TRUE);
+INSERT INTO burgers (name, devoured) VALUES ("cheeseburger", TRUE);
+INSERT INTO burgers (name, devoured) VALUES ("double cheeseburger", FALSE);
+INSERT INTO burgers (name, devoured) VALUES ("bacon cheeseburger", TRUE);
+INSERT INTO burgers (name, devoured) VALUES ("jalepino cheeseburger", FALSE);
+INSERT INTO burgers (name, devoured) VALUES ("patty melt", FALSE);
+INSERT INTO burgers (name, devoured) VALUES ("steak sandwhich", FALSE);
+INSERT INTO burgers (name, devoured) VALUES ("bbq burger", FALSE);
